49c3133f0ff7ebabb32441d67e8336e03b4ae666
guilhermefnneto <46166514+guilhermefnneto@users.noreply.github.com>
2018-12-27 15:45:25 -0200

    Implementação da Repository da Store.

--- src/main/java/com/invillia/acme/repository/StoreRepository.java
+++ src/main/java/com/invillia/acme/repository/StoreRepository.java
@@ -1,1 +1,13 @@
+package com.invillia.acme.repository;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.invillia.acme.model.Store;
+
+public interface StoreRepository extends CrudRepository<Store, Long> {
+
+	List<Store> findByName(String name);
+	
+}
