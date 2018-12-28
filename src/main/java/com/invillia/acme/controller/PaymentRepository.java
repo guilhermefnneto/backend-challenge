69cc648cf1273f13f0af9ec39610b0e922c78637
guilhermefnneto <46166514+guilhermefnneto@users.noreply.github.com>
2018-12-28 11:38:10 -0200

    Delete PaymentRepository.java

--- src/main/java/com/invillia/acme/controller/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.invillia.acme.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.invillia.acme.model.Payment;
-
-public interface PaymentRepository extends CrudRepository<Payment, Long> {
-
-}
