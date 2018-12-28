d5b7409c3ab396d3f1596634b1932ce5c162ed61
guilhermefnneto <46166514+guilhermefnneto@users.noreply.github.com>
2018-12-28 11:41:12 -0200

    Gravando os serviços.

--- src/main/java/com/invillia/acme/service/OrderService.java
+++ src/main/java/com/invillia/acme/service/OrderService.java
@@ -1,9 +1,13 @@
 package com.invillia.acme.service;
 
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.invillia.acme.model.Order;
+import com.invillia.acme.model.OrderStatus;
+import com.invillia.acme.model.PaymentStatus;
 import com.invillia.acme.repository.OrderRepository;
 
 @Service
@@ -18,6 +22,23 @@ public class OrderService {
 	
 	public Order read(Order pOrder) {
 		return orderRepository.findById(pOrder.getId()).get();
+	}
+	
+	public boolean refund(Order pOrder) {
+		Calendar confirmation = pOrder.getConfirmation();
+		Calendar now = Calendar.getInstance();
+		
+		long millisConfirmation = confirmation.getTimeInMillis();
+		long millisNow = now.getTimeInMillis();
+		
+		int days = (int)((millisNow - millisConfirmation)/ (24*60*60*1000)); // hora em minutos em segundos em milésimos
+		
+		if (days <= 10 && pOrder.getPayment() != null && PaymentStatus.CONCLUSED.equals(pOrder.getPayment().getStatus())) {
+			pOrder.setStatus(OrderStatus.REFUND);
+			return orderRepository.save(pOrder) != null;
+		}
+		
+		return false;
 	}
 		
 }
